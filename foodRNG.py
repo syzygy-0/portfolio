753575191b68e6291d0e3da270dde996cf97a7ed
Chelsea Wright <143695183+syzygy-0@users.noreply.github.com>
2023-09-27 21:04:04 +0000

    Add files via upload
    Boyfriend asked me to create a program that can help select a dinner for us when we can't decide. It's a very simple program containing a list of possible dinners, and uses the random module to select from the list

---
+++ foodRNG.py
@@ -0,0 +1,6 @@
+import random
+
+foods = ["big salad", "pizza and salad", "fish", "stir fry", "pasta", "tacos"]
+print("The list of foods is ", foods)
+dinner = random.choice(foods)
+print("For dinner, let's have ", dinner, "!")
